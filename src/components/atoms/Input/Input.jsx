3852775db56193843a43d119b9077fbf2fdba812
MyeongHwan Kim <lostalgia.kr@gmail.com>
2026-01-31 20:41:40 +0900

    Login Page 완성

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,13 +1,10 @@
-// src/components/atoms/Input/Input.jsx
 'use client';
 
 import { forwardRef } from 'react';
 import styles from './Input.module.css';
 
-const Input = forwardRef(function Input({ type = 'text', className, ...props }, ref) {
-  const resolvedClassName = className ? className : (styles.input ?? '');
-
-  return <input ref={ref} type={type} className={resolvedClassName} {...props} />;
+const Input = forwardRef(function Input({ type = 'text', className = '', ...props }, ref) {
+  return <input ref={ref} type={type} className={[styles.input, className].join(' ')} {...props} />;
 });
 
 export default Input;
